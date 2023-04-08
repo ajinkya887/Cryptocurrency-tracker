5785a873bb027269dff59996ce5f76c33da0830d
ajinkya sontakke <ajinkyasontakke07@gmail.com>
2023-04-08 21:29:38 +0530

    added individual page & made responsive

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -18,45 +18,77 @@ const Crypto = () => {
   const coin = coins?.find((item) => {
     return item.id === id
   })
-
-
   return (
     <main>
       <div className='context'>
         <p className='coin-name'>{coin?.name.toUpperCase()}</p>
         <img className='img' src={coin?.image} />
-        <p className='list'>Coin Symbol: {coin?.symbol}</p>
-        <p className='list'>Current Price: $ {coin?.current_price} </p>
-        <p className='list'>Market Rank: {coin?.market_cap_rank.toLocaleString()}</p>
-        <p className='list'>Market Capital: $ {coin?.market_cap} </p>
-        <p className='list'>Highest Of Past 24hr: $ {coin?.high_24h}</p>
-        <p className='list'>Lowest Of Past 24hr: $ {coin?.low_24h}</p>
-        {coin?.price_change_24h < 0 ? (
-          <p className='list-red'>Price Change In 24hr: $ {coin?.price_change_24h.toFixed(2)}</p>
-        ) : (
-          <p className='list-green'>Price Change In 24hr: $ {coin?.price_change_24h.toFixed(2)}</p>
-        )}
-
-        {coin?.price_change_percentage_24h < 0 ? (
-          <p className='list-red'>Price Change (%) In 24hr: {coin?.price_change_percentage_24h.toFixed(2)}</p>
-        ) : (
-          <p className='list-green'>Price Change (%) In 24hr: {coin?.price_change_percentage_24h.toFixed(2)}</p>
-        )}
-
-        {coin?.market_cap_change_24h < 0 ? (
-          <p className='list-red'>Price Change In 24hr: $ {coin?.market_cap_change_24h.toFixed(2)}</p>
-        ) : (
-          <p className='list-green'>Price Change In 24hr: $ {coin?.market_cap_change_24h.toFixed(2)}</p>
-        )}
-
-        {coin?.market_cap_change_percentage_24h < 0 ? (
-          <p className='list-red'>Price Change (%) In 24hr: {coin?.market_cap_change_percentage_24h.toFixed(2)}</p>
-        ) : (
-          <p className='list-green'>Price Change (%) In 24hr: {coin?.market_cap_change_percentage_24h.toFixed(2)}</p>
-        )}
-
-        <p className='list'>Total Volume: {coin?.total_volume}</p>
-        <p className='list'>Last Updated: {coin?.last_updated}</p>
+        <table>
+          <tr >
+            <td className='list'>Coin Symbol</td>
+            <td className='list'>:  {coin?.symbol} </td>
+          </tr>
+          <tr>
+            <td className='list'>Current Price</td>
+            <td className='list'>: $ {coin?.current_price}</td>
+          </tr>
+          <tr>
+            <td className='list'>Market Rank</td>
+            <td className='list'>: {coin?.market_cap_rank.toLocaleString()}</td>
+          </tr>
+          <tr>
+            <td className='list'>Market Capital</td>
+            <td className='list'>: $ {coin?.market_cap}</td>
+          </tr>
+          <tr>
+            <td className='list'>Highest Of Past 24hr</td>
+            <td className='list'>: $ {coin?.high_24h}</td>
+          </tr>
+          <tr>
+            <td className='list'>Lowest Of Past 24hr</td>
+            <td className='list'>: $ {coin?.low_24h}</td>
+          </tr>
+          <tr>
+            <td className='list'>Price Change In 24hr</td>
+            <td>{coin?.price_change_24h < 0 ? (
+              <p className='list-red'>: $ {coin?.price_change_24h.toFixed(2)}</p>
+            ) : (
+              <p className='list-green'>: $ {coin?.price_change_24h.toFixed(2)}</p>
+            )}</td>
+          </tr>
+          <tr>
+            <td className='list'>Price Change (%) In 24hr</td>
+            <td>{coin?.price_change_percentage_24h < 0 ? (
+              <p className='list-red'>: {coin?.price_change_percentage_24h.toFixed(2)}</p>
+            ) : (
+              <p className='list-green'>: {coin?.price_change_percentage_24h.toFixed(2)}</p>
+            )}</td>
+          </tr>
+          <tr>
+            <td className='list'>Price Change In Market Cap (24hr)</td>
+            <td>{coin?.market_cap_change_24h < 0 ? (
+              <p className='list-red'>: $ {coin?.market_cap_change_24h.toFixed(2)}</p>
+            ) : (
+              <p className='list-green'>: $ {coin?.market_cap_change_24h.toFixed(2)}</p>
+            )}</td>
+          </tr>
+          <tr>
+            <td className='list'>Price Change (%) In Market Cap (24hr)</td>
+            <td>{coin?.market_cap_change_percentage_24h < 0 ? (
+              <p className='list-red'>: {coin?.market_cap_change_percentage_24h.toFixed(2)}</p>
+            ) : (
+              <p className='list-green'>: {coin?.market_cap_change_percentage_24h.toFixed(2)}</p>
+            )}</td>
+          </tr>
+          <tr>
+            <td className='list'>Total Volume</td>
+            <td className='list'>: {coin?.total_volume}</td>
+          </tr>
+          <tr>
+            <td className='list'>Last Updated</td>
+            <td className='list'>: {coin?.last_updated}</td>
+          </tr>
+        </table>
       </div>
       <div className="area" >
         <ul className="circles">
